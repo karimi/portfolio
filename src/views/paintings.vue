72fefd48befadabbb14c79015e3ec8922a6f36c6
Morteza Karimi <2769101+karimi@users.noreply.github.com>
2025-07-29 20:48:37 -0700

    Titles of paintings

--- src/views/paintings.vue
+++ src/views/paintings.vue
@@ -1,115 +1,171 @@
 <template>
     <Carousel :slideContent="[
         {
-            route: 'welcome',
+            route: 'clove',
             content: [
             h('b',  'Clove'),
-            h('img', { src: cloveImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-            h('p', { class: 'text-sm font-light' }, 'This is some description for Clove.')
+            h('img', { src: cloveImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: '300am',
             content: [
-                h('b', '3:00 AM'),
-                h('img', { src: am300Img, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for 3:00 AM.')
+            h('b', '3:00 AM'),
+            h('img', { src: am300Img, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'atrain',
             content: [
-                h('b', 'A Train'),
-                h('img', { src: atrainImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for A Train.')
+            h('b', 'A Train'),
+            h('img', { src: atrainImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'cat',
             content: [
-                h('b', 'Cat'),
-                h('img', { src: catImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Cat.')
+            h('b', 'Cat'),
+            h('img', { src: catImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'fredrickstead',
             content: [
-                h('b', 'Fredrickstead'),
-                h('img', { src: fredricksteadImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Fredrickstead.')
+            h('b', 'Fredrickstead'),
+            h('img', { src: fredricksteadImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'jaguar',
             content: [
-                h('b', 'Jaguar'),
-                h('img', { src: jaguarImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Jaguar.')
+            h('b', 'Jaguar'),
+            h('img', { src: jaguarImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'lullaby',
             content: [
-                h('b', 'Lullaby'),
-                h('img', { src: lullabyImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Lullaby.')
+            h('b', 'Lullaby'),
+            h('img', { src: lullabyImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'maman',
             content: [
-                h('b', 'Maman'),
-                h('img', { src: mamanImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Maman.')
+            h('b', 'Maman'),
+            h('img', { src: mamanImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'mesimeri',
             content: [
-                h('b', 'Mesimeri'),
-                h('img', { src: mesimeriImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Mesimeri.')
+            h('b', 'Mesimeri'),
+            h('img', { src: mesimeriImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'onice',
             content: [
-                h('b', 'On Ice'),
-                h('img', { src: oniceImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for On Ice.')
+            h('b', 'On Ice'),
+            h('img', { src: oniceImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'raw',
             content: [
-                h('b', 'Raw'),
-                h('img', { src: rawImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Raw.')
+            h('b', 'Raw'),
+            h('img', { src: rawImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'rise',
             content: [
-                h('b', 'Rise'),
-                h('img', { src: riseImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Rise.')
+            h('b', 'Rise'),
+            h('img', { src: riseImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'utah',
             content: [
-                h('b', 'Utah'),
-                h('img', { src: utahImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for Utah.')
+            h('b', 'Utah'),
+            h('img', { src: utahImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         },
         {
             route: 'wearethesea',
             content: [
-                h('b', 'We Are The Sea'),
-                h('img', { src: wearetheseaImg, class: 'w-full h-[40vh] object-cover', style: 'max-width:100%; max-height:60vh;' }),
-                h('p', { class: 'text-sm font-light' }, 'This is some description for We Are The Sea.')
+            h('b', 'We Are The Sea'),
+            h('img', { src: wearetheseaImg, class: 'w-full object-contain', style: 'max-width:100%; max-height:60vh;' }),
+            h('div', { style: 'display: flex; justify-content: space-between; gap: 1rem;' }, [
+                h('p', { class: 'text-sm ' }, 'Guache on paper'),
+                h('p', { class: 'text-sm ' }, '24 x 18 inches'),
+            ]),
+            h('p', { class: 'text-sm font-light', style: 'margin-left:auto;' }, '2025')
             ]
         }
     ]" />
