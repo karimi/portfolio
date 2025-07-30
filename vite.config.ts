3c02450707f4a0a3d2d5f7e31d552bc51bc520b3
Morteza Karimi <2769101+karimi@users.noreply.github.com>
2025-07-29 21:50:20 -0700

    Add comment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/portfolio/', //Needed if directly serving from githib pages without custom domain
+  // base: '/portfolio/', //For serving from karimi.github.io/portfolio
   base: '/', // Use root for custom domain
   plugins: [vue()],
 })
