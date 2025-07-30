49efbbd8ac20f379b3b252855c1dcb38109130a3
Morteza Karimi <2769101+karimi@users.noreply.github.com>
2025-07-29 21:49:45 -0700

    Use / for base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/portfolio/', //Needed if directly serving from githib pages without custom domain
+  base: '/', // Use root for custom domain
   plugins: [vue()],
 })
